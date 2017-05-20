1aab882495628563e667564d996359e7a6ead2bb
Dénes Párkányi <pdenes86@gmail.com>
2017-05-20 14:29:36 +0200

    Finish user edit, update, index, and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,7 @@
 class SessionsController < ApplicationController
 
   def new
+  
   end
 
   # def create
@@ -22,7 +23,8 @@ class SessionsController < ApplicationController
       if user && user.authenticate(params[:session][:password])
         log_in user
         params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-        redirect_to user
+        redirect_back_or user
+
       else
         flash.now[:danger] = 'Invalid email/password combination'
         render 'new'
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,6 +1,10 @@
 
 require 'test_helper'
 
+def setup
+    @user = users(:michael)
+  end
+
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
   test "layout links" do
@@ -13,5 +17,14 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", signup_path
     get contact_path
     assert_select "title", full_title("Contact")
+    get signup_path
+    assert_select "title", full_title('Sign up')
+
+    #log_in_as(@user)
+    # follow_redirect!
+    # assert_select "a[href=?]", users_path
+    # assert_select "a[href=?]", logout_path
+    # assert_select "a[href=?]", edit_user_path(@user)
+    # assert_select "a[href=?]", user_path(@user)
   end
 end
