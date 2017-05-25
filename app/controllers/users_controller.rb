a61899c92a8ba9fdc836679d68576c5cf9e0fa82
Dénes Párkányi <pdenes86@gmail.com>
2017-05-25 18:55:33 +0200

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,8 @@ before_action :admin_user,     only: :destroy
 
   def show
     @user = User.find(params[:id])
-    redirect_to root_url and return unless @user.activated?
+  #  redirect_to root_url and return unless @user.activated?
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
 
@@ -65,14 +66,8 @@ before_action :admin_user,     only: :destroy
 
      # Before filters
 
-     # Confirms a logged-in user.
-     def logged_in_user
-       unless logged_in?
-         store_location
-         flash[:danger] = "Please log in."
-         redirect_to login_url
-       end
-     end
+    #  Confirms a logged-in user. Moved to app controller
+
 
 
       # Confirms the correct user.
