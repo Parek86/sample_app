e5635cc4fed9a7a7693d3108eeb848c382dff0cc
Dénes Párkányi <pdenes86@gmail.com>
2017-05-30 07:28:39 +0200

    Add reaction component-3

--- app/controllers/reactions_controller.rb
+++ app/controllers/reactions_controller.rb
@@ -2,14 +2,25 @@ class ReactionsController < ApplicationController
 before_action :logged_in_user, only: [:create]
 
 def create
-    @reaction=Reaction.new(params[:reaction])
-    @reaction.save
+    @reaction= Reaction.new(reaction_params)
+    # @reaction= Reaction.new(params[:reaction])
+    @reaction.user_id=current_user.id
+    @reaction.micropost_id=10
+    if @reaction.save
     flash[:success] = "Reaction!!!"
     redirect_to root_url
+  else
+    flash[:danger] = "No reaction"
+    redirect_to root_url
+  end
 end
 
 private
 
+def reaction_params
+  params.require(:reaction).permit(:content,:user_id,:micropost_id)
 
+
+end
 
 end
--- app/models/reaction.rb
+++ app/models/reaction.rb
@@ -1,8 +1,8 @@
 class Reaction < ApplicationRecord
   belongs_to :micropost
   belongs_to :user
-#  VALID_CONTENT=["Like", "Dislike", "Desire", "None"]
-  validates :content, presence: true   #, inclusion: { in: VALID_CONTENT, message: "%{value} is not a valid reaction"}
+  VALID_CONTENT=["Like", "Dislike", "Desire", "None"]
+  validates :content, presence: true,   inclusion: { in: VALID_CONTENT, message: "%{value} is not a valid reaction"}
   validates :micropost_id, presence: true
   validates :user_id, presence: true
 end
